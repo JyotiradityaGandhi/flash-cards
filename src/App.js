5c77143ce35739d4d72f36614882d30daa30dc90
Jyotiraditya Gandhi <jyotiraditya602@gmail.com>
2023-12-04 00:18:04 +0530

    deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const questions = [
+  {
+    id: 1,
+    que: "What language is React based on?",
+    ans: "Javascript",
+  },
+  {
+    id: 2,
+    que: "What are the building blocks of React apps?",
+    ans: "Components",
+  },
+  {
+    id: 3,
+    que: "How to pass data from parent to child components?",
+    ans: "Props",
+  },
+  {
+    id: 4,
+    que: "What's the name of the syntax we use to describe UI in React?",
+    ans: "JSX",
+  
+  },
+  {
+    id: 5,
+    que: "How to give components memory?",
+    ans: "State",
+    
+  },
+  {
+    id: 6,
+    que: "What do we call an input element that is completely synchronised with state?",
+    ans: "Controlled Elements",
+
+  },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <FlashCards />
     </div>
   );
 }
 
-export default App;
+function FlashCards() {
+  const [selectedId, setSelectedId] = useState(0);
+  return (
+    <div className="cardlist">
+      {questions.map((question) => (
+        <div onClick={() => {selectedId === question.id ? setSelectedId(0) : setSelectedId(question.id) }} className={selectedId === question.id ? "card ans" : "card que"}>
+        <span>{selectedId === question.id ? question.ans : question.que}</span>
+      </div>
+      ))}
+    </div>
+  );
+}
+
